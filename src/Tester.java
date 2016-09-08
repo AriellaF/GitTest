ef5a522625d68e14e4b4c61d29abf9918f0489a1
ariella_fuzaylov <ariellafuzaylov@gmail.com>
2016-09-08 14:05:42 -0400

    asf

--- src/Tester.java
+++ src/Tester.java
@@ -1,7 +1,8 @@
-package PACKAGE_NAME;
-
 /**
  * Created by ariella_fuzaylov on 9/8/16.
  */
 public class Tester {
+    public static void main(String[] args) {
+        System.out.println("Fuck this");
+    }
 }
